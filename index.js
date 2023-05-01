1bda17435dca27e22b41c28fc35e3bec663be546
sakibbiswas <sakibsakib99880@gmail.com>
2023-05-01 13:15:15 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -3,13 +3,40 @@ const app = express()
 const cors = require('cors')
 
 const port = process.env.PORT || 4000;
-const Categories=require('./data/Categories.json')
+const Categories = require('./data/Categories.json')
+const news = require('./data/news.json')
+
 app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Dragon is running')
 })
 app.get('/Categories', (req, res) => {
   res.send(Categories)
+})
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectednews = news.find(n => n._id === id)
+    res.send(selectednews)
+    console.log(id);
+ 
+})
+
+app.get('/Categories/:id', (req, res) => { 
+    const id =parseInt( req.params.id);
+     console.log(id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+          const categorynews = news.filter(n =>parseInt( n.category_id) === id)
+    res.send(categorynews) 
+    }
+   
+ 
 })
 
 app.listen(port, () => {
